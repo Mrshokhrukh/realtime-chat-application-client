bae645d7dcbdc595b3cd87ad3a7a7abe5f2cf33f
MusobekErgashev <musobeccoder007@gmail.com>
2026-03-25 17:42:08 +0500

    Layout page + main design structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Menu from "@/components/Menu";
+import Header from "@/components/Header";
+import Aside from "@/components/Aside";
+import LeftAside from "@/components/LeftAside";
 
 export const metadata: Metadata = {
   title: "Aether Chat",
@@ -16,7 +20,17 @@ export default function RootLayout({
       lang="en"
       className={`h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full w-full flex">
+        <Menu />
+        <div className='w-full flex flex-col bg-[#101017]'>
+          <Header />
+          <div className="flex justify-between">
+            <LeftAside />
+            {children}
+            <Aside />
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
