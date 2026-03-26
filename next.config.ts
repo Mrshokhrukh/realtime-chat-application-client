42b1bbeb668bcd3184fcfd1552edf82d69483212
MusobekErgashev <musobeccoder007@gmail.com>
2026-03-26 10:20:16 +0500

    Header redesigned + clean code

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.tubefilter.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+      },
+    ],
+  },
   async redirects() {
+
     return [
       {
         source: '/',
